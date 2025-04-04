d53855672976ff51997cce77e5f3f4e1f7a4c4ff
Bill Dean <742designs@gmail.com>
2025-04-04 14:41:38 -0400

    Code cleanup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,6 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  [1, 2, 3].map((num) => {
-    return num * 2;
-  });
-
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-16 sm:items-start">
