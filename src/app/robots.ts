95272bf4220e8b34a00f34748ea1eaba14adb690
Bill Dean <742designs@gmail.com>
2025-06-04 23:06:10 -0400

    Formatting tweaks after Prettier update.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,12 +3,12 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            allow: "/",
-            disallow: [],
-            userAgent: "*",
-        },
-        sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
-    };
+  return {
+    rules: {
+      allow: "/",
+      disallow: [],
+      userAgent: "*",
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+  };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,10 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    return [
-        {
-            lastModified: new Date(),
-            url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
-        },
-    ];
+  return [
+    {
+      lastModified: new Date(),
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
+    },
+  ];
 }
