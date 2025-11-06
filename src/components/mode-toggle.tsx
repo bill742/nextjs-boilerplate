a8da50397f340f7561de19ee3837beda29498166
Bill Dean <742designs@gmail.com>
2025-11-06 17:34:03 -0500

    Fix for hydration error.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,7 +2,7 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import * as React from "react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -14,6 +14,13 @@ import {
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <TooltipProvider>
