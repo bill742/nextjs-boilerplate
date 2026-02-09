3aef8ce23e9ef7ed60d58cee84145b1c5b46b2f3
Bill Dean <742designs@gmail.com>
2026-02-09 16:39:08 -0500

    Fix typo.

--- src/components/skip-nav.tsx
+++ src/components/skip-nav.tsx
@@ -1,6 +1,6 @@
 const SkipNav = () => (
   <a
-    className="h-1px w-1px absolute top-auto left-2500 overflow-hidden focus:static focus:h-auto focus:w-auto"
+    className="h-1px w-1px absolute top-auto -left-2500 overflow-hidden focus:static focus:h-auto focus:w-auto"
     href="#main"
   >
     Skip to main content
