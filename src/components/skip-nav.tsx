0c8706fdcf8b973951393ec484d1bf2713e33c86
Bill Dean <742designs@gmail.com>
2026-02-09 15:30:10 -0500

    Add SkipNav component for improved accessibility and include it in RootLayout

---
+++ src/components/skip-nav.tsx
@@ -0,0 +1,12 @@
+const SkipNav = () => (
+  <a
+    className="h-1px w-1px absolute top-auto left-2500 overflow-hidden focus:static focus:h-auto focus:w-auto"
+    href="#main"
+  >
+    Skip to main content
+  </a>
+);
+
+SkipNav.displayName = "SkipNav";
+
+export default SkipNav;
